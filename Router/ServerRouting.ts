59a9b51afb503a22e7df5aec700966dc3107f9c3
vs.puhanov <vs.puhanov@tensor.ru>
2018-11-20 15:06:11 +0300

    ServerRouting should calculate app name based on request path, not originalUrl

--- Router/ServerRouting.ts
+++ Router/ServerRouting.ts
@@ -1,22 +1,9 @@
 /// <amd-module name="Router/ServerRouting" />
-// @ts-ignore
-import getUrl = require('Transport/URL/getUrl');
 
 import RouterHelper from 'Router/Helper';
 
-function _prepareRequest(request) {
-   if (!process.domain) {
-      process.domain = {
-         enter: () => { },
-         exit: () => { }
-      };
-   }
-   process.domain.req = request;
-}
-
 function getAppName(request) {
-   _prepareRequest(request);
-   return RouterHelper.getAppNameByUrl(getUrl());
+   return RouterHelper.getAppNameByUrl(request.path);
 }
 
 function renderApp(request, response, appName) {
