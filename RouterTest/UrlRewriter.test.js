5042a4ef2ec70c80d2def54f82960daed6fbc0d5
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2019-03-11 08:11:34 +0300

    adds UrlRewriter reverse tests

--- RouterTest/UrlRewriter.test.js
+++ RouterTest/UrlRewriter.test.js
@@ -30,6 +30,32 @@ function(Router, json) {
             url: '/a/b/c/d/e',
             result: '/abcd/e'
          }
+      ],
+      testsReverse = [
+         {
+            url: '/OnlineSbisRu',
+            result: '/'
+         },
+         {
+            url: '/a',
+            result: '/a'
+         },
+         {
+            url: '/ab',
+            result: '/a/b'
+         },
+         {
+            url: '/ab/c',
+            result: '/a/b/c'
+         },
+         {
+            url: '/abcd',
+            result: '/a/b/c/d'
+         },
+         {
+            url: '/abcd/e',
+            result: '/a/b/c/d/e'
+         }
       ];
 
    function runTests(testArr) {
@@ -38,49 +64,15 @@ function(Router, json) {
       });
    }
 
+   function runTestsReverse(testArr) {
+      testArr.forEach(function(test) {
+         assert.equal(UrlRewriter.getReverse(test.url), test.result);
+      });
+   }
+
    describe('Router/UrlRewriter', function() {
       beforeEach(function() {
          UrlRewriter._prepareRoutes(json);
-      });
-
-      it('rewrites the url', function() {
-         runTests(tests);
-      });
-
-      it('ignores query in the url', function() {
-         var queryString = '?paramA=true&paramB=false',
-            queryTests = tests.map(function(test) {
-               return {
-                  url: test.url + queryString,
-                  result: test.result + queryString
-               };
-            });
-
-         runTests(queryTests);
-      });
-
-      it('ignores hash in the url', function() {
-         var hashString = '#userid=00000111',
-            hashTests = tests.map(function(test) {
-               return {
-                  url: test.url + hashString,
-                  result: test.result + hashString
-               };
-            });
-
-         runTests(hashTests);
-      });
-
-      it('ignores query and hash in the url if both are present', function() {
-         var combinedString = '?paramA=true&paramB=false#userid=00000111',
-            combinedTests = tests.map(function(test) {
-               return {
-                  url: test.url + combinedString,
-                  result: test.result + combinedString
-               };
-            });
-
-         runTests(combinedTests);
       });
 
       it('does not throw if routes.json did not load', function() {
@@ -89,17 +81,114 @@ function(Router, json) {
          assert.doesNotThrow(UrlRewriter._prepareRoutes.bind(UrlRewriter, {}));
       });
 
-      it('returns urls as is if routes.json did not load', function() {
-         UrlRewriter._prepareRoutes(null);
-
-         var resEqUrl = tests.map(function(test) {
-            return {
-               url: test.url,
-               result: test.url
-            };
+      describe('forward rewrite', function() {
+         it('rewrites the url', function() {
+            runTests(tests);
          });
 
-         runTests(resEqUrl);
+         it('ignores query in the url', function() {
+            var queryString = '?paramA=true&paramB=false',
+               queryTests = tests.map(function(test) {
+                  return {
+                     url: test.url + queryString,
+                     result: test.result + queryString
+                  };
+               });
+
+            runTests(queryTests);
+         });
+
+         it('ignores hash in the url', function() {
+            var hashString = '#userid=00000111',
+               hashTests = tests.map(function(test) {
+                  return {
+                     url: test.url + hashString,
+                     result: test.result + hashString
+                  };
+               });
+
+            runTests(hashTests);
+         });
+
+         it('ignores query and hash in the url if both are present', function() {
+            var combinedString = '?paramA=true&paramB=false#userid=00000111',
+               combinedTests = tests.map(function(test) {
+                  return {
+                     url: test.url + combinedString,
+                     result: test.result + combinedString
+                  };
+               });
+
+            runTests(combinedTests);
+         });
+
+         it('returns urls as is if routes.json did not load', function() {
+            UrlRewriter._prepareRoutes(null);
+
+            var resEqUrl = tests.map(function(test) {
+               return {
+                  url: test.url,
+                  result: test.url
+               };
+            });
+
+            runTests(resEqUrl);
+         });
+      });
+
+      describe('reverse rewrite', function() {
+         it('rewrites the url', function() {
+            runTestsReverse(testsReverse);
+         });
+
+         it('ignores query in the url', function() {
+            var queryString = '?paramA=true&paramB=false',
+               queryTests = testsReverse.map(function(test) {
+                  return {
+                     url: test.url + queryString,
+                     result: test.result + queryString
+                  };
+               });
+
+            runTestsReverse(queryTests);
+         });
+
+         it('ignores hash in the url', function() {
+            var hashString = '#userid=00000111',
+               hashTests = testsReverse.map(function(test) {
+                  return {
+                     url: test.url + hashString,
+                     result: test.result + hashString
+                  };
+               });
+
+            runTestsReverse(hashTests);
+         });
+
+         it('ignores query and hash in the url if both are present', function() {
+            var combinedString = '?paramA=true&paramB=false#userid=00000111',
+               combinedTests = testsReverse.map(function(test) {
+                  return {
+                     url: test.url + combinedString,
+                     result: test.result + combinedString
+                  };
+               });
+
+            runTestsReverse(combinedTests);
+         });
+
+         it('returns urls as is if routes.json did not load', function() {
+            UrlRewriter._prepareRoutes(null);
+
+            var resEqUrl = testsReverse.map(function(test) {
+               return {
+                  url: test.url,
+                  result: test.url
+               };
+            });
+
+            runTestsReverse(resEqUrl);
+         });
       });
    });
 });
