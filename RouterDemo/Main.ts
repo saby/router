16c54ddf614a7edc4277092cc1ab54575cecc404
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2019-04-08 09:55:22 +0300

    manually init request storage

--- RouterDemo/Main.ts
+++ RouterDemo/Main.ts
@@ -4,11 +4,24 @@
 import * as Control from 'Core/Control';
 // @ts-ignore
 import template = require('wml!RouterDemo/Main');
+// @ts-ignore
+import * as AppInit  from 'Application/Initializer';
 
 import 'css!RouterDemo/Main';
 
 class Main extends Control {
    public _template: Function = template;
+
+   constructor(cfg) {
+      super();
+
+      // Initialize the Request storage that is used by Router/Data.
+      // This is usually done by Application/Core, which is not used
+      // in these demos
+      if (!AppInit.isInit()) {
+         AppInit.default(cfg);
+      }
+   }
 }
 
 export = Main;
