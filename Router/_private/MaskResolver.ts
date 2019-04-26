93446578588222241c0cbb3e9514eedc1f4ff6ce
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2019-04-26 07:29:49 +0300

    changed URIError comment

--- Router/_private/MaskResolver.ts
+++ Router/_private/MaskResolver.ts
@@ -294,8 +294,10 @@ function _decodeParam(param: string): string {
       try {
          result = decodeURIComponent(result);
       } catch (e) {
-         // URIError, it was possibly already decoded,
-         // ignore the error
+         // If decoder throws an error, that means that the original
+         // URL was malformed. If the user enters an invalid URL,
+         // ignore the decoding (because it can't be decoded by the
+         // browser) and return the string as is.
       }
    }
    return result;
