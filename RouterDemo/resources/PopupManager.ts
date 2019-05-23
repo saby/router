c71af77cc49e515b47fc3ef16af8e61cc9c5f37f
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2019-05-23 11:13:25 +0300

    adds Popup routing demo

---
+++ RouterDemo/resources/PopupManager.ts
@@ -0,0 +1,49 @@
+import Control = require('Core/Control');
+import template = require('wml!RouterDemo/resources/PopupManager');
+
+import 'css!RouterDemo/resources/PopupManager';
+
+interface IPopupConfig {
+    openerId: string;
+    template: string;
+    closeHandler?: Function;
+    config?: Record<string, unknown>;
+}
+
+export default class PopupManager extends Control {
+    _template: Function = template;
+
+    private _items: any;
+    private _registeredItems: string[];
+
+    protected _beforeMount(): void {
+        this._items = {
+            _version: 0
+        };
+        this._registeredItems = [];
+    }
+
+    protected _beforeUnmount(): void {
+        this._items = null;
+    }
+
+    private _requestedOpen(e: Event, cfg: IPopupConfig): void {
+        console.log(cfg);
+        this._items[cfg.openerId] = cfg;
+        if (!this._registeredItems.includes(cfg.openerId)) {
+            this._registeredItems.push(cfg.openerId);
+        }
+        this._items._version++;
+    }
+
+    private _requestedClose(e: Event, cfg: { openerId: string }): void {
+        console.log(cfg);
+        delete this._items[cfg.openerId];
+        this._registeredItems.splice(this._registeredItems.indexOf(cfg.openerId), 1);
+        this._items._version++;
+    }
+
+    private _userClosed(e: Event, openerId: string): void {
+        this._items[openerId].closeHandler();
+    }
+}
---
+++ RouterDemo/resources/SamplePopup.ts
@@ -0,0 +1,35 @@
+import Control = require('Core/Control');
+import template = require('wml!RouterDemo/resources/SamplePopup');
+
+import SampleOpener from './SampleOpener';
+
+interface ISamplePopupOptions {
+    popupDepth: number;
+    isRecursive: boolean;
+    left: number;
+    top: number;
+}
+
+interface ISamplePopupChildren {
+    opener: SampleOpener;
+}
+
+class SamplePopup extends Control {
+    protected _options: ISamplePopupOptions;
+    protected _children: ISamplePopupChildren;
+
+    _template: Function = template;
+
+    private _urlAddedOrChanged(e: Event, newParam: string) {
+        this._children.opener.open({
+            template: 'RouterDemo/resources/SamplePopup',
+            itemId: newParam,
+            top: 150,
+            left: this._options.left + 20,
+            isRecursive: this._options.isRecursive,
+            popupDepth: this._options.popupDepth + 1
+        });
+    }
+}
+
+export = SamplePopup;
