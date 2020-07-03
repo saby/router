2f2fde1428b1f55b818cb7b3ca0ddd1554ab971e
li.mustafin <>
2020-07-03 16:23:31 +0300

    https://online.sbis.ru/opendoc.html?guid=8d622a59-3886-4034-935e-b5d7b074ed74
    правки в тестах браузера

--- Router/_private/Reference.ts
+++ Router/_private/Reference.ts
@@ -206,6 +206,7 @@ class Reference extends Control implements IRegisterableComponent {
     private _recalcHref(cfg: IReferenceOptions): void {
         this._state = MaskResolver.calculateHref(cfg.state, cfg);
         if (cfg.href) {
+            cfg.replace = true;
             this._href = MaskResolver.calculateHref(cfg.href, cfg);
         } else {
             this._href = getReverse(this._state);
