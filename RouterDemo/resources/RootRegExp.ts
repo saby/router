244a4daa173d0a18cf605554482ca92d4c2a0b54
li.mustafin <>
2020-10-29 17:50:58 +0300

    https://online.sbis.ru/opendoc.html?guid=1c096262-66c5-45ae-8120-25457ed6b101
    страница демонстрации регулярки в router.json

---
+++ RouterDemo/resources/RootRegExp.ts
@@ -0,0 +1,25 @@
+/// <amd-module name="RouterDemo/resources/RootRegExp" />
+/**
+ * @author Мустафин Л.И.
+ */
+
+// @ts-ignore
+import * as Control from 'Core/Control';
+// @ts-ignore
+import template = require('wml!RouterDemo/resources/RootRegExp');
+
+/**
+ * Демонстрация регулярного выражения в router.json
+ */
+
+class RootRegExp extends Control {
+   _template: Function = template;
+   protected _docId: string = '';
+
+   protected _beforeMount(options: {docId: string}): void {
+      this._docId = options.docId;
+   }
+
+}
+
+export = RootRegExp;
