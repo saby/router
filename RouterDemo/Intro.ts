00780b89767785ae2536b3391b79f31761224f89
li.mustafin <>
2020-10-29 16:36:47 +0300

    https://online.sbis.ru/opendoc.html?guid=1c096262-66c5-45ae-8120-25457ed6b101
    демонстрация запрета SPA переходов

--- RouterDemo/Intro.ts
+++ RouterDemo/Intro.ts
@@ -1,10 +1,11 @@
 /// <amd-module name="RouterDemo/Intro" />
 /**
- * @author Санников К.А.
+ * @author Мустафин Л.И.
  */
 
 // @ts-ignore
 import * as Control from 'Core/Control';
+import { Controller } from 'Router/router';
 // @ts-ignore
 import template = require('wml!RouterDemo/Intro');
 
@@ -14,6 +15,29 @@ import template = require('wml!RouterDemo/Intro');
 
 class Intro extends Control {
    _template: Function = template;
+   protected _preventNavigateMessage: string = '';
+   protected _preventNavigate: boolean = false;
+
+   protected _beforeMount(options: {}): void {
+      Controller.addRoute(this, (newState, currentState): Promise<boolean> => {
+         return new Promise((resolve, reject) => {
+            if (this._preventNavigate) {
+               reject();
+               return;
+            }
+            resolve();
+         });
+      });
+   }
+
+   _setPreventNavigate(): void {
+      this._preventNavigate = !this._preventNavigate;
+      if (!this._preventNavigate) {
+         this._preventNavigateMessage = '';
+      } else {
+         this._preventNavigateMessage = 'SPA переходы заблокированы.';
+      }
+   }
 }
 
 export = Intro;
