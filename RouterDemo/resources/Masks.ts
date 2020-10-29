7418e35e124c42df482183b3e67ee741e2dbd92a
li.mustafin <>
2020-10-29 18:30:49 +0300

    https://online.sbis.ru/opendoc.html?guid=1c096262-66c5-45ae-8120-25457ed6b101
    демонстрация query и fragment масок

---
+++ RouterDemo/resources/Masks.ts
@@ -0,0 +1,19 @@
+/// <amd-module name="RouterDemo/resources/Masks" />
+/**
+ * @author Мустафин Л.И.
+ */
+
+// @ts-ignore
+import * as Control from 'Core/Control';
+// @ts-ignore
+import template = require('wml!RouterDemo/resources/Masks');
+
+/**
+ * Демонстрация query и fragment параметров в маске
+ */
+
+class Masks extends Control {
+   _template: Function = template;
+}
+
+export = Masks;
