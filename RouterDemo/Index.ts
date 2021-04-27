cc2f74e57e6f06ae8eec3edd5f33f7fad476408b
li.mustafin <>
2021-04-27 17:48:46 +0300

    https://online.sbis.ru/opendoc.html?guid=95c8c500-9f1d-40ca-aed8-d9e19d66662f

--- RouterDemo/Index.ts
+++ RouterDemo/Index.ts
@@ -1,5 +1,4 @@
 import {Control, TemplateFunction} from 'UI/Base';
-import { getConfig } from 'Application/Env';
 import * as template from 'wml!RouterDemo/Index';
 
 /**
@@ -7,12 +6,4 @@ import * as template from 'wml!RouterDemo/Index';
  */
 export default class Index extends Control {
    protected _template: TemplateFunction = template;
-   /**
-    * Опция для изменения шаблона демки при построении в div
-    */
-   protected bootstrapWrapperMode: boolean = false;
-
-   protected _beforeMount(): void {
-      this.bootstrapWrapperMode = getConfig('bootstrapWrapperMode') as boolean;
-   }
 }
