50b25443d5e55db414652200310265101781eb9f
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2024-04-11 18:01:50 +0300

    new change

---
+++ Router/_ServerRouting/Interfaces/IModuleLoader.ts
@@ -0,0 +1,43 @@
+import { IRouter } from 'Router/router';
+import { IControlConstructor } from 'UICore/Base';
+import { IPageSourceNotFound } from './IPageSource';
+import { AdaptiveModeType } from 'UI/Adaptive';
+
+type IDefaultControl = IControlConstructor & {
+    getDataToRender?: (
+        url: string,
+        params?: { prerender?: boolean; adaptiveMode?: AdaptiveModeType }
+    ) => Promise<unknown>;
+};
+
+/**
+ * В модуле, который строится на странице может быть метод getDataToRender.
+ * Этот метод вернет данные для страницы.
+ * @private
+ */
+export interface IModuleToRender {
+    default: IDefaultControl;
+    getDataToRender?: (
+        url: string,
+        params: { prerender?: boolean; adaptiveMode?: AdaptiveModeType },
+        Router: IRouter
+    ) => Promise<unknown>;
+}
+
+/**
+ * Интерфейс результата микропроцесса загрузки модуля - если модуль существует и его удалось загрузить
+ * @private
+ */
+export interface IModuleFound {
+    loadStatus: 'success';
+    module: IModuleToRender;
+}
+
+/**
+ * Интерфейс результата микропроцесса загрузки модуля - если модуля НЕ существует и его НЕ удалось загрузить
+ * @private
+ */
+export interface IModuleNotFound {
+    loadStatus: 'not_found';
+    notFound: IPageSourceNotFound;
+}
