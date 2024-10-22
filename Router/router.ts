c118a0f76955f3ccddca6833e736df1cf97d04e8
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2024-10-22 18:02:09 +0300

    new change

---
+++ Router/router.ts
@@ -0,0 +1,67 @@
+/**
+ * Библиотека single-page роутинга
+ * Более подробно API роутинга описано по {@link /doc/platform/developmentapl/interface-development/routing/#router-api ссылке}
+ * @library
+ * @public
+ * @author Мустафин Л.И.
+ * @module
+ */
+
+import { getRootRouter, createNewRouter, IRouter } from './_private/Router/Router';
+import initOnPopState from './_private/Router/InitOnPopState';
+initOnPopState(getRootRouter);
+
+import * as Controller from './_private/_deprecated/Controller';
+import * as Data from './_private/_deprecated/Data';
+import {
+    IHistoryState,
+    IRegisterableComponent,
+    TStateChangeFunction,
+    TOnChangeHistoryState,
+} from './_private/DataInterfaces';
+import { IRouterUrl } from './_private/Router/RouterUrl';
+import { IMaskResolver } from './_private/MaskResolver';
+import { IHistory } from './_private/History';
+import * as History from './_private/_deprecated/History';
+import * as MaskResolver from './_private/_deprecated/MaskResolver';
+import { getAppNameByUrl } from './_private/MaskResolver';
+import UrlRewriterClass, { IUrlRewriter } from './_private/UrlRewriter';
+
+/**
+ * @cfg {Router/router:IUrlRewriter} Набор методов для работы с router.json, в котором можно задать соответствие
+ * между текущим путем и его короткой записью - "красивым" URL
+ * @public
+ */
+export const UrlRewriter = UrlRewriterClass.getInstance();
+
+export {
+    default as Reference,
+    IReferenceProps,
+    ISyntheticMouseEvent,
+    TReferenceProps,
+    IReferenceState,
+} from './_private/Reference';
+export { default as Route, IRouteProps, TGetDataToRender, IProgressBar } from './_private/Route';
+export { default as Context } from './_private/context/Context';
+export { default as ContextProvider, IContextProps } from './_private/context/ContextProvider';
+export { useRouter } from './_private/context/useRouter';
+export { withRouter } from './_private/context/withRouter';
+
+export {
+    Controller,
+    Data,
+    History,
+    MaskResolver,
+    getAppNameByUrl,
+    getRootRouter,
+    createNewRouter,
+    IHistoryState,
+    IRegisterableComponent,
+    TStateChangeFunction,
+    TOnChangeHistoryState,
+    IRouter,
+    IRouterUrl,
+    IUrlRewriter,
+    IMaskResolver,
+    IHistory,
+};
